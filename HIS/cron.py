b9d9d556b1dfa6e56fe5d1ab6e430f540b62c947
AntonioTan <congmi@outlook.com>
2020-04-20 20:19:36 +0800

    CRONJOB for some time schedule jobs

---
+++ HIS/cron.py
@@ -0,0 +1,14 @@
+from appoint import models
+
+
+def test():
+    print('cronb test!')
+
+
+def test1():
+    new_doctor = models.Doctor.objects.create(name='test',
+                                 sex=1,
+                                 title='test',
+                                 strength='test',
+                                 department=models.Department.objects.all()[0],)
+    new_doctor.save()
