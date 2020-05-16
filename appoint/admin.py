80a86f834b384d55118d5e05f93643d596db32d2
AntonioTan <congmi@outlook.com>
2020-05-16 23:23:14 +0800

    Create fake data into database

--- appoint/admin.py
+++ appoint/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Registration)
+# admin.site.register(models.Registration)
 admin.site.register(models.Department)
 admin.site.register(models.Doctor)
 admin.site.register(models.Order)
--- appoint/models.py
+++ appoint/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-
 # Create your models here.
 
 
-class Registration(models.Model):
-    # type 1 for clinic || 0 for non-clinic
-    type = models.BooleanField()
-    # start_time will be used for checking whether this registration is valid
-    # a registration is valid for 4 hour from start time
-    start_time = models.DateTimeField()
-    price = models.SmallIntegerField()
-    doctor = models.ForeignKey(to='Doctor', on_delete=models.CASCADE)
-    available = models.BooleanField()
+# class Registration(models.Model):
+#     # type 1 for clinic || 0 for non-clinic
+#     type = models.BooleanField()
+#     # start_time will be used for checking whether this registration is valid
+#     # a registration is valid for 4 hour from start time
+#     start_time = models.DateTimeField()
+#     price = models.SmallIntegerField()
+#     doctor = models.ForeignKey(to='Doctor', on_delete=models.CASCADE)
+#     available = models.BooleanField()
 
 
 class Department(models.Model):
@@ -32,19 +31,23 @@ class Doctor(models.Model):
 
 class Order(models.Model):
     patient = models.ForeignKey(to='login.User', on_delete=models.CASCADE)
-    registration = models.ForeignKey(to='Registration', on_delete=models.CASCADE)
+    registration = models.ForeignKey(to='Schedule', on_delete=models.CASCADE)
     order_time = models.DateTimeField(null=True)
-    # 1 for valid || 0 for invalid
-    valid = models.BooleanField()
+    # 1 for to_purchase || 2 for purchased || 3 for completed || 4 for against_rule
+    status = models.SmallIntegerField(default=1)
 
 
 class Schedule(models.Model):
-    doctor = models.ForeignKey(to='Doctor', on_delete=models.CASCADE)
-    morning_registration_num = models.SmallIntegerField(default=0)
-    afternoon_registration_num = models.SmallIntegerField(default=0)
+    doctor = models.ForeignKey(to='Doctor', on_delete=models.CASCADE, null=True)
+    department = models.ForeignKey(to='Department', on_delete=models.CASCADE)
+    num = models.SmallIntegerField(default=0)
+    # 0 for morning 1 for afternoon
+    morning_afternoon = models.BooleanField(default=0)
+    # 1 for Monday| 2 for Tuesday
     weekday = models.SmallIntegerField()
-    # type 1 for clinic || 0 for non-clinic
+    # type 1 for doctor || 0 for non-doctor
     type = models.BooleanField()
+    price = models.SmallIntegerField(default=80)
 
 
 
